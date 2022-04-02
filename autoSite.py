e4843dffe60f66667bdb7a2f0340615351c08bdc
Gabe Chavez <73260097+GabeCodes72@users.noreply.github.com>
2022-04-01 22:45:19 -0500

    Some minor changes
    Just sweeping the monsters' hair under the bed. Making it look nicer.

--- autoSite.py
+++ autoSite.py
@@ -1,5 +1,5 @@
 #gonna have a doc that has the set list of values. Defaulting at the standard 8 45-minute-periods.
-#Block periods (Wed and Thu) will be 1 hour and 30 minutes by default
+#Block periods (Wed and Thu) will be 1 hour and 30 minutes by default, except 5th/4th on block, as they are used for lunch.
 #this doc will serve as the customization menu, making the values typed define the required values in this file.
 from datetime import time
 import autoSiteSystems as ass
@@ -50,18 +50,18 @@ if ass.dotw == 4:
         print("It's passing period!")
         sleep(5)
 else:
-    if ass.is_time_between(time(7,49), time(8,37)):
+    if ass.is_time_between(time(8,00), time(8,45)):
         print("It's 1st Hour.")
         ass.firstHour()
-    elif ass.is_time_between(time(8,40), time(9,28)):
+    elif ass.is_time_between(time(8,50), time(9,35)):
         print("It's 2nd Hour.")
         ass.secHour()
-    elif ass.is_time_between(time(9,31), time(10,19)):
+    elif ass.is_time_between(time(9,40), time(10,25)):
         print("It's 3rd Hour.")
         ass.thirdHour()
-    elif ass.is_time_between(time(10,24), time(11,9)):
+    elif ass.is_time_between(time(10,30), time(11,15)):
         print("It's AI!")
-    elif ass.is_time_between(time(11,13), time(12,28)):
+    elif ass.is_time_between(time(11,20), time(12,28)):
         print("It's 4th Hour.")
         ass.fourHour()
     elif ass.is_time_between(time(12,33), time(13,19)):
